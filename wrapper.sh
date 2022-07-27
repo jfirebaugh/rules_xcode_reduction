5783b79c3d26fb48afc175ec0b6669713a1dd151
John Firebaugh <john.firebaugh@gmail.com>
2022-07-26 22:01:30 -0700

    Working

--- wrapper.sh
+++ wrapper.sh
@@ -1,2 +1,9 @@
-args=("$@")
-bazel "${args[0]}" --compilation_mode=dbg "${args[@]:1}"
+case "$1" in
+build | run | test)
+  args=("$@")
+  bazel "${args[0]}" --compilation_mode=dbg "${args[@]:1}"
+  ;;
+*)
+  bazel "$@"
+  ;;
+esac
