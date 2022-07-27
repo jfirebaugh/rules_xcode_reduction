65d33cda50082cf781178d92c18ae7d5a602f4e5
John Firebaugh <john.firebaugh@gmail.com>
2022-07-26 22:11:06 -0700

    Test case for https://github.com/buildbuddy-io/rules_xcodeproj/issues/828

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+#include "tools/cpp/runfiles/runfiles.h"
+#include <iostream>
+
+using bazel::tools::cpp::runfiles::Runfiles;
+
 int main() {
+    std::string error;
+    std::unique_ptr<Runfiles> runfiles(Runfiles::CreateForTest(&error));
+
+    if (runfiles == nullptr) {
+        std::cerr << error << std::endl;
+        return 1;
+    }
+
+    std::cout << runfiles->Rlocation("rules_xcode_reduction/data.txt") << std::endl;
     return 0;
 }
--- wrapper.sh
+++
@@ -1,9 +0,0 @@
-case "$1" in
-build | run | test)
-  args=("$@")
-  bazel "${args[0]}" --compilation_mode=dbg "${args[@]:1}"
-  ;;
-*)
-  bazel "$@"
-  ;;
-esac
